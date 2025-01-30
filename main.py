fb925d50787878bba42cfe6660f4d782e30f9110
Steven <steven.schlechte@gmx.net>
2025-01-30 19:46:25 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main():
+    """ Main program """
+    # Code goes over here.
+    return 0
+
+if __name__ == "__main__":
+    main()
+class Schedule:
+    pass
+
+class CreateNewSchedule():
+    def init(self, data: dict, dayes: dict,):
+        self.data = data
+
+    def createSchedule() -> Schedule:
+        pass
+    
+    def checkSchedule() -> bool:
+        pass
+    def check_if_slot_is_valid() -> bool:
+        pass
+    def check_if_everybody_gets_equaly_hours() -> bool:
+        pass
+    def check_if_everybody_gets_hours_with_friends() -> bool:
+        pass
+    def check_if_everybody_gets_only_hours_where_they_can() -> bool:
+        pass
+    def check_if_one_person_is_female() -> bool:
+        pass
+    def check_if_every_trait_is_once() -> bool:
+        pass
